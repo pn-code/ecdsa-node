a7b770cf01e3516a3863f9e3dc0a96cf60def473
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-08 16:54:29 -0700

    Created getAddress & reorganized generateKeys

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -1,10 +1,23 @@
 const { secp256k1 } = require("ethereum-cryptography/secp256k1.js");
+const { keccak256 } = require("ethereum-cryptography/keccak.js");
 const { toHex } = require("ethereum-cryptography/utils");
 
-const privateKey = secp256k1.utils.randomPrivateKey();
+// Generate Keys
+function generateKeys() {
+  const privateKey = secp256k1.utils.randomPrivateKey();
+  const publicKey = secp256k1.getPublicKey(privateKey);
 
-console.log('private key', toHex(privateKey));
+  return { public: publicKey, private: privateKey };
+}
 
-const publicKey = secp256k1.getPublicKey(privateKey);
+// Get the Ethereum Address
+function getAddress(publicKey) {
+  return keccak256(publicKey.slice(1)).slice(-20);
+}
 
-console.log("public key", toHex(publicKey))
+const keys = generateKeys();
+const address = getAddress(keys.public);
+
+console.log("private key", toHex(keys.private));
+console.log("public key", toHex(keys.public));
+console.log("public address", toHex(address));
